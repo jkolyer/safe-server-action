7f35568a68ef119fafe308e015fbe408bacd2bb3
Jonathan Kolyer <jonathankolyer@gmail.com>
2024-12-08 10:02:10 -0800

    safe-server-action code converion: remove references to nextjs; keep only package files

--- src/action-builder.ts
+++ src/action-builder.ts
@@ -1,6 +1,8 @@
 import { deepmerge } from "deepmerge-ts";
-import { isNotFoundError } from "next/dist/client/components/not-found.js";
-import { isRedirectError } from "next/dist/client/components/redirect.js";
+import { isNotFoundError } from "./not-found.js";
+import { isRedirectError } from "./is-redirect-error.js";
+// import { isNotFoundError } from "next/dist/client/components/not-found.js";
+// import { isRedirectError } from "next/dist/client/components/redirect.js";
 import type {} from "zod";
 import type { Infer, InferArray, InferIn, InferInArray, Schema, ValidationAdapter } from "./adapters/types";
 import type {
